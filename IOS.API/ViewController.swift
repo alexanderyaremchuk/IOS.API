0814ddeff41d38298c2a67f74ee6634efa2fca98
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2016-11-01 09:52:12 +0100

    Initial Commit

---
+++ IOS.API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS.API
+//
+//  Created by Alexander Yaremchuk on 11/1/16.
+//  Copyright (c) 2016 Qeueuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
