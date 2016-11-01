1a565d304bf308a6d8c99ae62f017419389f33cd
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2016-11-01 17:04:13 +0100

    more stuff

--- QueueService.swift
+++ QueueService.swift
@@ -16,11 +16,22 @@ public class QueueService {
     static let API_ROOT = "http://%@.test-q.queue-it.net/api/queue"
     
     public func enqueue(customerId: String, eventId: String, userId: String, sdkVersion: String, layoutName: String, language: String,
-        success: (status: QueueStatus) -> Void, failure: (failureData: QueueServiceFailureData ) -> Void) {
+        success: (status: QueueStatus) -> Void,
+        failure: (failureData: QueueServiceFailureData ) -> Void) {
         
         
     }
     
+    public func submitRequestWithURL(url: NSURL, httpMethod: String, bodyDict: NSDictionary, expectedStatus: Int,
+        success: (data: NSData) -> Void,
+        failure: (failureData: QueueServiceFailureData ) -> Void) -> String {
+            return "";
+    }
     
+    public func submitPUTPath(path: String, bodyDict: NSDictionary, success: (data: NSData) -> Void, failure: (failureData: QueueServiceFailureData ) -> Void) -> String {
+        var url = NSURL(fileURLWithPath: path)
+        return self.submitRequestWithURL(url, "PUT", bodyDict, 200, success, failure)
+        
+    }
     
 }
